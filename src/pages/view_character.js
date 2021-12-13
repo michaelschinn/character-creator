66cd44161c95636cfaeb23090d2af5a370698b1e
michael.stephen.chinn <michael.stephen.chinn@gmail.com>
2021-12-13 14:59:04 -0500

    Styled the view character page.

--- src/pages/view_character.js
+++ src/pages/view_character.js
@@ -2,38 +2,43 @@ import {Link} from 'react-router-dom';
 export default function ViewCharacter(){
     return (
         <div className="viewCharacter">
-            <h1>View Character</h1>
-            <h2>Jagra Deathstrike</h2>
             <div className="sheet">
                 <div className="charImage">
-
+                    <div className="charTitle">
+                        <h2>Jagra Deathstrike</h2>
+                        <p className="genderClass">Male&nbsp;Assassin</p>
+                    </div>
+                    <div className="charData">
+                        <p>
+                            Strength: 5
+                            <br />
+                            Vitality: 3
+                            <br />
+                            Dexterity: 10
+                            <br />
+                            Intelligence: 8
+                        </p>
+                        <p>
+                            HP: 30
+                            <br />
+                            MP: 80
+                            <br />
+                            Damage Bonus: 10
+                            <br />
+                            Accuracy: 75%
+                            <br />
+                            Defense: 20
+                            <br />
+                            Spell Potency: 80%
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    <p>Male&nbsp;Assasin</p>
-                    <p>
-                        Strength: 5
-                        <br />
-                        Vitality: 3
-                        <br />
-                        Dexterity: 10
-                        <br />
-                        Intelligence: 8
-                        <br />
-                        HP: 30
-                        <br />
-                        MP: 80
-                        <br />
-                        Damage Bonus: 10
-                        <br />
-                        Accuracy: 75%
-                        <br />
-                        Defense: 20
-                        <br />
-                        Spell Potency: 80%
-                    </p>
+                <div className="charControl">
+                    <Link to="/manually_create"><button className="backButton">Back</button></Link>
+                    <Link to="/manually_create"><button className="editButton">Edit</button></Link>
+                    <Link to="/manually_create"><button className="saveButton">Save</button></Link>
                 </div>
             </div>
-            <Link to="/manually_create"><button className="backButton">Back</button></Link>
         </div>
     );
 }
